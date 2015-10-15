8f6a313120199815a0c43a2e1b1ab577c3e5d985
kimdasom <kimdasom@dasom>
2015-10-15 16:38:43 +0900

    first upload

---
+++ src/kr/ac/kookmin/shape/MoreThread.java
@@ -0,0 +1,27 @@
+package kr.ac.kookmin.shape;
+
+public class MoreThread {
+
+	
+	public class MoreThreads {
+		public void main(String args[]) {
+		System.out.println("Main thread starting.");
+		MyThread mt1 = new MyThread("Child #1");
+		MyThread mt2 = new MyThread("Child #2");
+		MyThread mt3 = new MyThread("Child #3");
+		for(int i=0; i < 50; i++) {
+		System.out.print(".");
+		try {
+		Thread.sleep(100);
+		}
+		catch(InterruptedException exc) {
+		System.out.println("Main thread interrupted.");
+		}
+		}
+		System.out.println("Main thread ending.");
+		}
+		}
+	
+	
+	
+}
